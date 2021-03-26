a92781a181f4d2a9ecdc0c97fdcccd40c23b360b
Bijoy Das <dasbijoy9804417767@gmail.com>
2021-03-26 21:20:31 +0530

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # collegehelper
+
+# First cd/go to to your project directry via cmd the run the command npm start
+
+# Now go to your browser and type https://localhost:3000 and hit enter
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require("path");
+
+/**************** INITIAL MODULES SETUP ***********************/
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+/************* Static files ***********/
+
+app.use(express.static(path.join(__dirname , "public")));
+
+/********** SETTING UP ROUTES *************/
+
+app.use("/" , require("./server/router/router.js"));
+
+
+
+
+
+
+
+
+
+
+/******** Port setup **********/
+
+app.listen(3000 , () => {
+  console.log("Server started on port 3000");
+})
