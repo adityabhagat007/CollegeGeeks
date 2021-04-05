6c53fe01b34cb4ff446b662993b66d047f89aa67
adityabhagat007 <rajarambhagat6582@gmail.com>
2021-04-06 04:33:28 +0530

    adding frontend files

--- server/router/router.js
+++ server/router/router.js
@@ -3,5 +3,16 @@ const route = require("express").Router();
 route.get("/" , (req, res) => {
   res.render("index");
 });
-
+route.get("/Signup" , (req, res) => {
+  res.render("Signup");
+});
+route.get("/about" , (req, res) => {
+  res.render("");
+});
+route.get("/login" , (req, res) => {
+  res.render("login");
+});
+route.get("/home" , (req, res) => {
+  res.render("home");
+});
 module.exports = route;
