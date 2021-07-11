1655a968a0224070968631ec6cb0a43374f38cb3
Bijoy Das <dasbijoy9804417767@gmail.com>
2021-07-11 22:21:01 +0530

    Added the question and answer model

--- controllers/feed.js
+++ controllers/feed.js
@@ -13,6 +13,18 @@ exports.getHomePage = (req, res, next) => {
 exports.getMyAccount = (req, res, next) => {
   res.render("myaccount");
 };
+
 exports.getQuestionPage = (req, res, next) => {
   res.render("questionPage");
+};
+
+exports.postAskQuestion = async (req, res, next) => {
+  try {
+    //Pulling the data out from request body
+    const statement = req.body.statement;
+    const category = req.body.category;
+    //
+  } catch (err) {
+    next(err);
+  }
 };
---
+++ models/answer.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const answerSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    authorName: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Answer", answerSchema);
