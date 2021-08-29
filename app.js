d1750092fe0d952f279a25d0a57dbf6684ddb67f
adityabhagat007 <rajarambhagat6582@gmail.com>
2021-08-29 10:43:22 +0530

    Cores setup

--- app.js
+++ app.js
@@ -48,6 +48,23 @@ app.use(
 /************* Flash Setup ***********/
 app.use(flash());
 
+
+/**** CORES Settings ****/
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
+
+
+
 /************* Static files ***********/
 
 app.use(express.static(path.join(__dirname, "public")));
