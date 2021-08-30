cdc6c81aa09289bdd84aee63c62f8070affb38e1
adityabhagat007 <rajarambhagat6582@gmail.com>
2021-08-30 18:31:32 +0530

    fixed minor bug

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -16,7 +16,7 @@ const multer = require("multer");
 // });
 const cloudinary = require("cloudinary").v2;
 const {CloudinaryStorage} =require('multer-storage-cloudinary');
-const multer = require("multer");
+
 
 cloudinary.config({
   cloud_name: process.env.COULDINARY_CLOUD_NAME,
