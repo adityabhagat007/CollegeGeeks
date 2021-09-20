3c2e430bdd0c2cadd5623eb5cc042673297e3442
adityabhagat007 <rajarambhagat6582@gmail.com>
2021-09-20 21:24:39 +0530

    restricting website to open in http

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ const authRoutes = require("./routes/auth");
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 app.set("view engine", "ejs");
 
 app.use(
